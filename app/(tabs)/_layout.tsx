6dff320092d2e24aad1326e9ff578fb374458694
shubh-mp3 <shubh.mehta2019@gmail.com>
2025-03-06 17:11:52 +0000

    applied prettier

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,12 @@
-import { Drawer } from 'expo-router/drawer';
-import React from 'react';
-import { Platform } from 'react-native';
+import { Drawer } from "expo-router/drawer";
+import React from "react";
+import { Platform } from "react-native";
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { HapticTab } from "@/components/HapticTab";
+import { IconSymbol } from "@/components/ui/IconSymbol";
+import TabBarBackground from "@/components/ui/TabBarBackground";
+import { Colors } from "@/constants/Colors";
+import { useColorScheme } from "@/hooks/useColorScheme";
 
 export default function DrawerLayout() {
   const colorScheme = useColorScheme();
@@ -14,27 +14,29 @@ export default function DrawerLayout() {
   return (
     <Drawer
       screenOptions={{
-        drawerActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: true, 
-        headerTitleAlign: 'center'
-      }}>
+        drawerActiveTintColor: Colors[colorScheme ?? "light"].tint,
+        headerShown: true,
+        headerTitleAlign: "center",
+      }}
+    >
       <Drawer.Screen
-          name="index" // This is the name of the page and must match the url from root
-          options={{
-            drawerLabel: 'Home',
-            title: 'Username/Password Generator',
-            drawerIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-          }}
-
-        
+        name="index" // This is the name of the page and must match the url from root
+        options={{
+          drawerLabel: "Home",
+          title: "Username/Password Generator",
+          drawerIcon: ({ color }) => (
+            <IconSymbol size={28} name="house.fill" color={color} />
+          ),
+        }}
       />
       <Drawer.Screen
         name="explore"
         options={{
-          drawerIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          drawerIcon: ({ color }) => (
+            <IconSymbol size={28} name="paperplane.fill" color={color} />
+          ),
         }}
       />
     </Drawer>
   );
 }
-
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,81 +1,88 @@
-import { Image, StyleSheet, Platform, Button } from 'react-native';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-import {useState} from 'react';
+import { Image, StyleSheet, Platform, Button } from "react-native";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import { useState } from "react";
 
 export default function HomeScreen() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const [visible, setVisible] = useState(true);
   async function getPassword() {
     try {
-      const response = await fetch('http://localhost:3000/password'); 
+      const response = await fetch("http://localhost:3000/password");
       if (!response.ok) {
-        console.log('Error:', response.status);
+        console.log("Error:", response.status);
       }
-  
+
       const json = await response.json();
       setPassword(json.password);
       console.log(json);
     } catch (error: any) {
-      console.log('Error:', error.message)
-    console.log(password)
-    setPassword(password);
-  }
+      console.log("Error:", error.message);
+      console.log(password);
+      setPassword(password);
+    }
   }
   async function getUsername() {
     try {
-      const response = await fetch('http://localhost:3000/username'); 
+      const response = await fetch("http://localhost:3000/username");
       if (!response.ok) {
-        console.log('Error:', response.status);
+        console.log("Error:", response.status);
       }
-  
+
       const json = await response.json();
       setUsername(json.username);
       console.log(json);
     } catch (error: any) {
-      console.log('Error:', error.message)
-    console.log(username)
-    setUsername(username);
+      console.log("Error:", error.message);
+      console.log(username);
+      setUsername(username);
+    }
   }
-  }
-  
-  
+
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
+          source={require("@/assets/images/partial-react-logo.png")}
           style={styles.reactLogo}
         />
-      }>
+      }
+    >
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Welcome!</ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Try my Username generator</ThemedText>
         <ThemedText>
-         Press the button to generate a password
-         <Button title="Generate Username" onPress={() =>getUsername()} />
+          Press the button to generate a password
+          <Button title="Generate Username" onPress={() => getUsername()} />
         </ThemedText>
         <ThemedText>
           <ThemedText type="defaultSemiBold">Username:{username}</ThemedText>
         </ThemedText>
         <ThemedText type="subtitle">Try my password generator</ThemedText>
         <ThemedText>
-         Press the button to generate a password
-         <Button title="Generate Password" onPress={() =>getPassword()} />
+          Press the button to generate a password
+          <Button title="Generate Password" onPress={() => getPassword()} />
         </ThemedText>
         <ThemedText>
-          {visible?<ThemedText type="defaultSemiBold">Password:{password}</ThemedText>: <ThemedText type="defaultSemiBold">Password:********</ThemedText>}
+          {visible ? (
+            <ThemedText type="defaultSemiBold">Password:{password}</ThemedText>
+          ) : (
+            <ThemedText type="defaultSemiBold">Password:********</ThemedText>
+          )}
         </ThemedText>
         <ThemedText>
-          <Button title="Hide password" onPress={() =>setVisible(!visible)} />
+          <Button title="Hide password" onPress={() => setVisible(!visible)} />
         </ThemedText>
         <ThemedText>
-          <Button title="Copy" onPress={() =>navigator.clipboard.writeText(password)} />
+          <Button
+            title="Copy"
+            onPress={() => navigator.clipboard.writeText(password)}
+          />
         </ThemedText>
       </ThemedView>
     </ParallaxScrollView>
@@ -84,8 +91,8 @@ export default function HomeScreen() {
 
 const styles = StyleSheet.create({
   titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     gap: 8,
   },
   stepContainer: {
@@ -97,7 +104,6 @@ const styles = StyleSheet.create({
     width: 290,
     bottom: 0,
     left: 0,
-    position: 'absolute',
+    position: "absolute",
   },
 });
-
